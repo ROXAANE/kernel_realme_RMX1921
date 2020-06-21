d2d10285e407ee4bd85e2e5e973e21ab843ad07b
Advaith Bhat <bajithottyadvaith99@gmail.com>
2020-06-21 07:16:59 +0000

    Import Build script
    Signed-off-by: ROXAANE <bajithottyadvaith99@gmail.com>

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+clear
+echo SeniXa Kernel Build Tool
+echo By Advaith Bhat
+echo Please Sit Back And Chill...
+export MAIN=`readlink -f ../`
+export KERNELDIR=`readlink -f .`
+rm $KERNELDIR/zip/Image.gz-dtb
+rm $KERNELDIR/zip/kernel_output.txt
+make ARCH=arm64 RMX1921_defconfig
+make -j$(nproc --all) ARCH=arm64 \
+        CC="$MAIN/kernel/proton-clang/bin/clang" \
+        CROSS_COMPILE="$MAIN/kernel/proton-clang/bin/aarch64-linux-gnu-" \
+        CROSS_COMPILE_ARM32="$MAIN/kernel/proton-clang/bin/arm-linux-gnueabi-"
+mv $KERNELDIR/arch/arm64/boot/Image.gz-dtb $KERNELDIR/zip/Image.gz-dtb
+echo ""
+echo "Compile Done"
+echo ""
+cd $KERNELDIR/zip
+zip -r SeniXa-RMX1921-Release-`date +%Y%m%d_%H%M`.zip * -x "kernel_output.txt" "*.zip"
+cd $KERNELDIR/
+echo "Zip Done"
